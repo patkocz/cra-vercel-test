84542524d67b255611a10a011b110d38b05f17fc
JCOMMERCE\patk <pawel.tkocz@jcommerce.pl>
2021-07-16 17:34:24 +0200

    develop branch

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <h1>HELLO WORLD</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis veritatis aliquam dolorem quae recusandae consequatur rem quibusdam nam, illum et culpa impedit nihil exercitationem eum amet sequi id reprehenderit eveniet, iste labore fugit sit adipisci? Culpa, beatae! Mollitia deserunt libero nesciunt eligendi, totam quo, ex velit nostrum aspernatur soluta iure? Neque suscipit consectetur quae repellat quibusdam dolorem optio enim, amet deserunt aspernatur rerum quod omnis architecto quos corporis provident quam atque, repudiandae eaque quasi mollitia, perferendis fuga. Pariatur consequatur ab eos, atque, blanditiis nemo excepturi autem sed at tempora soluta voluptatibus velit itaque dicta? Quam, nostrum! Sint quam optio delectus.</p>
+          <p>Develop branch</p>
         </p>
       </header>
     </div>
